b307579771c6af48c5f43f5a601a70f4c04955bf
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2021-05-16 15:07:10 +0300

    Add files via upload

---
+++ Functions in C/Find Max Function.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/* function declaration */
+int max(int num1, int num2);
+ 
+int main () {
+
+   /* local variable definition */
+   int a,b;
+   printf("This is a simple program to create a function and call it in C\n");
+   printf("\n>Give the first value: ");
+   scanf("%d",&a);
+   printf("\n>Give the second value: ");
+   scanf("%d",&b);
+   int ret;
+ 
+   /* calling a function to get max value */
+   ret = max(a, b);
+ 
+   printf( "Max value is : %d\n", ret );
+ 
+   return 0;
+}
+ 
+/* function returning the max between two numbers */
+int max(int num1, int num2) {
+
+   /* local variable declaration */
+   int result;
+ 
+   if (num1 > num2){
+      result = num1;
+   }
+   else if(num1<num2){
+      result = num2;
+   }
+   else
+   {
+      printf("Equal Value");
+   }
+   return result; 
+}
